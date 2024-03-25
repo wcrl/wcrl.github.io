6cfc74c990044df0957e8dfa5a1d0defaf23db38
srijal30 <srijal30@stuy.edu>
2024-03-25 01:51:33 -0400

    removed uneccassary components

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -3,17 +3,17 @@ var dropdowns = document.getElementsByClassName("dropdown-btn");
 console.log(dropdowns.length);
 
 for (let i = 0; i > dropdowns.length; i++) {
-    console.log("hi");
+  console.log("hi");
 }
 
 
 for (let i = 0; i < dropdowns.length; i++) {
-    console.log("dropdown found");
-  dropdowns[i].addEventListener("click", function() {
+  console.log("dropdown found");
+  dropdowns[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display  = "none";
+      dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
     }
@@ -21,12 +21,13 @@ for (let i = 0; i < dropdowns.length; i++) {
 }
 
 
-function toggleDropdown(dropdown){
-    dropdown.classList.toggle("active");
-    var dropdownContent = dropdown.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display  = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
+function toggleDropdown(dropdown) {
+  console.log("opening dropdown");
+  dropdown.classList.toggle("active");
+  var dropdownContent = dropdown.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
 };
